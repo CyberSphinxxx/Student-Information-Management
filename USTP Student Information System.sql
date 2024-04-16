3007e40503fa1a7c9c63fcc8be2e65fd94ac0226
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-16 15:17:49 +0800

    USTP Student Information System SQL

--- USTP Student Information System.sql
+++ USTP Student Information System.sql
@@ -1,1 +1,15 @@
+CREATE DATABASE IF NOT EXISTS student_management;
+
+USE student_management;
+
+CREATE TABLE IF NOT EXISTS students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    First_Name VARCHAR(255) NOT NULL,
+    Last_Name VARCHAR(255) NOT NULL,
+    Course VARCHAR(255) NOT NULL,
+    Year_Level VARCHAR(50) NOT NULL,
+    StudentID VARCHAR(50) NOT NULL,
+    Phone_Number VARCHAR(15) NOT NULL
+);
+
 SELECT * FROM students;
