a5d1627be431992bbb629d69858e568b378298d7
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-04-16 13:34:21 +0800

    Update InformationManagement.py

--- InformationManagement.py
+++ InformationManagement.py
@@ -6,53 +6,61 @@ def createMainWindow():
     root = tk.Tk()
     root.title("Created by: John Lemar Gonzales")
 
-    appLabel = tk.Label(root, text="Student Information System", fg="#06a099", width=35)
+    appLabel = tk.Label(root, text="USTP Student Information System", fg="#06a099", width=35)
     appLabel.config(font=("Sylfaen", 30))
     appLabel.grid(row=0, columnspan=2, padx=(10,10), pady=(30, 0))
 
-    nameLabel = tk.Label(root, text="Enter your name", width=40, anchor='w',
-                         font=("Sylfaen", 12))
-    nameLabel.grid(row=1, column=0, padx=(10,0), pady=(30, 0))
-    collegeLabel = tk.Label(root, text="Enter your college", width=40, anchor='w',
-                            font=("Sylfaen", 12))
-    collegeLabel.grid(row=2, column=0, padx=(10,0))
-    phoneLabel = tk.Label(root, text="Enter your phone number", width=40, anchor='w',
-                          font=("Sylfaen", 12))
-    phoneLabel.grid(row=3, column=0, padx=(10,0))
-    courseLabel = tk.Label(root, text="Enter your course", width=40, anchor='w',
-                            font=("Sylfaen", 12))
-    courseLabel.grid(row=4, column=0, padx=(10,0))
+    firstNameLabel = tk.Label(root, text="Enter Your First Name:", width=40, anchor='w', font=("Sylfaen", 12))
+    firstNameLabel.grid(row=1, column=0, padx=(10,0), pady=(30, 0))
+    lastNameLabel = tk.Label(root, text="Enter Your Last Name:", width=40, anchor='w', font=("Sylfaen", 12))
+    lastNameLabel.grid(row=2, column=0, padx=(10,0))
+    courseLabel = tk.Label(root, text="Enter Your Course:", width=40, anchor='w', font=("Sylfaen", 12))
+    courseLabel.grid(row=3, column=0, padx=(10,0))
+    yearLevelLabel = tk.Label(root, text="Enter Your Year Level:", width=40, anchor='w', font=("Sylfaen", 12))
+    yearLevelLabel.grid(row=4, column=0, padx=(10,0))
+    studentIdLabel = tk.Label(root, text="Enter Your Student ID:", width=40, anchor='w', font=("Sylfaen", 12))
+    studentIdLabel.grid(row=5, column=0, padx=(10,0))
+    phoneNumberLabel = tk.Label(root, text="Enter Your Phone Number:", width=40, anchor='w', font=("Sylfaen", 12))
+    phoneNumberLabel.grid(row=6, column=0, padx=(10,0))
 
-    nameEntry = tk.Entry(root, width=30)
-    collegeEntry = tk.Entry(root, width=30)
-    phoneEntry = tk.Entry(root, width=30)
+    firstNameEntry = tk.Entry(root, width=30)
+    lastNameEntry = tk.Entry(root, width=30)
     courseEntry = tk.Entry(root, width=30)
+    yearLevelEntry = tk.Entry(root, width=30)
+    studentIdEntry = tk.Entry(root, width=30)
+    phoneNumberEntry = tk.Entry(root, width=30)
 
-    nameEntry.grid(row=1, column=1, padx=(0,10), pady=(30, 20))
-    collegeEntry.grid(row=2, column=1, padx=(0,10), pady=20)
-    phoneEntry.grid(row=3, column=1, padx=(0,10), pady=20)
-    courseEntry.grid(row=4, column=1, padx=(0,10), pady=20)
+    firstNameEntry.grid(row=1, column=1, padx=(0,10), pady=(30, 20))
+    lastNameEntry.grid(row=2, column=1, padx=(0,10), pady=20)
+    courseEntry.grid(row=3, column=1, padx=(0,10), pady=20)
+    yearLevelEntry.grid(row=4, column=1, padx=(0,10), pady=20)
+    studentIdEntry.grid(row=5, column=1, padx=(0,10), pady=20)
+    phoneNumberEntry.grid(row=6, column=1, padx=(0,10), pady=20)
 
-    button = tk.Button(root, text="Save Information", command=lambda: takeNameInput(root, nameEntry, collegeEntry, phoneEntry, courseEntry))
-    button.grid(row=5, column=0, pady=30)
+    button = tk.Button(root, text="Save Information", command=lambda: takeNameInput(root, firstNameEntry, lastNameEntry, courseEntry, yearLevelEntry, studentIdEntry, phoneNumberEntry))
+    button.grid(row=7, column=0, pady=30)
 
     displayButton = tk.Button(root, text="Display All Information", command=lambda: displayResults(root))
-    displayButton.grid(row=5, column=1)
+    displayButton.grid(row=7, column=1)
 
     root.mainloop()
 
-def takeNameInput(root, nameEntry, collegeEntry, phoneEntry, courseEntry):
-    username = nameEntry.get()
-    nameEntry.delete(0, tk.END)
-    collegeName = collegeEntry.get().upper()  # Convert to uppercase
-    collegeEntry.delete(0, tk.END)
-    phone = phoneEntry.get()
-    phoneEntry.delete(0, tk.END)
-    course = courseEntry.get().upper()  # Convert to uppercase
+def takeNameInput(root, firstNameEntry, lastNameEntry, courseEntry, yearLevelEntry, studentIdEntry, phoneNumberEntry):
+    firstName = firstNameEntry.get()
+    firstNameEntry.delete(0, tk.END)
+    lastName = lastNameEntry.get()
+    lastNameEntry.delete(0, tk.END)
+    course = courseEntry.get()
     courseEntry.delete(0, tk.END)
+    yearLevel = yearLevelEntry.get()
+    yearLevelEntry.delete(0, tk.END)
+    studentId = studentIdEntry.get()
+    studentIdEntry.delete(0, tk.END)
+    phoneNumber = phoneNumberEntry.get()
+    phoneNumberEntry.delete(0, tk.END)
 
     try:
-        cursor.execute(f"INSERT INTO {TABLE_NAME} ({STUDENT_NAME}, {STUDENT_COLLEGE}, {STUDENT_COURSE}, {STUDENT_PHONE}) VALUES (%s, %s, %s, %s)", (username, collegeName, course, phone))
+        cursor.execute(f"INSERT INTO {TABLE_NAME} (First_Name, Last_Name, Course, Year_Level, StudentID, Phone_Number) VALUES (%s, %s, %s, %s, %s, %s)", (firstName, lastName, course, yearLevel, studentId, phoneNumber))
         connection.commit()
         messagebox.showinfo("Success", "Data Saved Successfully.")
     except Exception as e:
@@ -62,25 +70,25 @@ def displayResults(root):
     secondWindow = tk.Toplevel(root)
     secondWindow.title("Display results")
 
-    appLabel = tk.Label(secondWindow, text="Student Management System",
-                        fg="#06a099", width=40)
+    appLabel = tk.Label(secondWindow, text="Student Management System", fg="#06a099", width=40)
     appLabel.config(font=("Sylfaen", 30))
     appLabel.pack()
 
     tree = ttk.Treeview(secondWindow)
-    tree["columns"] = ("one", "two", "three", "four")
+    tree["columns"] = ("one", "two", "three", "four", "five", "six")
 
-    tree.heading("one", text="Student Name")
-    tree.heading("two", text="College Name")
+    tree.heading("one", text="First Name")
+    tree.heading("two", text="Last Name")
     tree.heading("three", text="Course")
-    tree.heading("four", text="Phone Number")
+    tree.heading("four", text="Year Level")
+    tree.heading("five", text="Student ID")
+    tree.heading("six", text="Phone Number")
 
     try:
         cursor.execute(f"SELECT * FROM {TABLE_NAME}")
         i = 0
         for row in cursor.fetchall():
-            tree.insert('', i, text="Student " + str(row[0]),
-                        values=(row[1], row[2], row[3], row[4]))
+            tree.insert('', i, text="Student " + str(row[0]), values=(row[1], row[2], row[3], row[4], row[5], row[6]))
             i += 1
     except Exception as e:
         messagebox.showerror("Error", f"Error occurred: {str(e)}")
@@ -102,11 +110,6 @@ try:
     cursor = connection.cursor()
 
     TABLE_NAME = "students"
-    STUDENT_ID = "student_id"
-    STUDENT_NAME = "student_name"
-    STUDENT_COLLEGE = "student_college"
-    STUDENT_COURSE = "course"
-    STUDENT_PHONE = "student_phone"
 
     createMainWindow()
 
